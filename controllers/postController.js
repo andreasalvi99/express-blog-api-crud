3b133beeb437a70884d2ab4d2e9c61f1897f2493
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-03 14:34:28 +0100

    import routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,4 +8,48 @@ function index(req, res) {
   });
 }
 
-module.exports = { index };
+function show(req, res) {
+  const postId = req.params.id;
+
+  const searchedPost = posts.find((post) => post.id === parseInt(postId));
+
+  if (!searchedPost) {
+    res.status(404).json({
+      message: `Post ${postId} non trovato`,
+      success: false,
+    });
+  }
+
+  res.json({
+    results: searchedPost,
+    message: `Ecco il post ${postId}`,
+    success: true,
+  });
+}
+
+function store(req, res) {
+  res.json({
+    message: `Creato nuovo post`,
+    success: true,
+  });
+}
+
+function update(req, res) {
+  const postId = req.params.id;
+
+  res.json({
+    message: `post ${postId} modificato interamente`,
+    success: true,
+  });
+}
+
+function destroy(req, res) {
+  const postId = req.params.id;
+
+  res.json({
+    message: `post ${postId} eliminato`,
+    success: true,
+  });
+}
+
+module.exports = { index, show, store, update, destroy };
--- routers/posts.js
+++ routers/posts.js
@@ -3,53 +3,9 @@ const router = express.Router();
 const postController = require("../controllers/postController");
 
 router.get("/", postController.index);
-
-//^ Show
-router.get("/:id", (req, res) => {
-  const postId = req.params.id;
-
-  const searchedPost = posts.find((post) => post.id === parseInt(postId));
-
-  if (!searchedPost) {
-    res.status(404).json({
-      message: `Post ${postId} non trovato`,
-      success: false,
-    });
-  }
-
-  res.json({
-    results: searchedPost,
-    message: `Ecco il post ${postId}`,
-    success: true,
-  });
-});
-
-//^ Store
-router.post("/", (req, res) => {
-  res.json({
-    message: `Creato nuovo post`,
-    success: true,
-  });
-});
-
-//^ Update
-router.put("/:id", (req, res) => {
-  const postId = req.params.id;
-
-  res.json({
-    message: `post ${postId} modificato interamente`,
-    success: true,
-  });
-});
-
-//^ Destroy
-router.delete("/:id", (req, res) => {
-  const postId = req.params.id;
-
-  res.json({
-    message: `post ${postId} eliminato`,
-    success: true,
-  });
-});
+router.get("/:id", postController.show);
+router.post("/", postController.store);
+router.put("/:id", postController.update);
+router.delete("/:id", postController.destroy);
 
 module.exports = router;
